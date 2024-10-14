402f22ea4494494d6e2e80ce76b4d11cd002ca5b
jujp2466 <65272345+jujp2466@users.noreply.github.com>
2024-10-14 09:23:01 +0800

    測試畫面是否有變動

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 <template>
     <header>
         <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-        Testing CI/CD
+        Testing CI/CD-1014
         <!--<div class="wrapper">
           <HelloWorld msg="You did it!" />
         </div>-->
